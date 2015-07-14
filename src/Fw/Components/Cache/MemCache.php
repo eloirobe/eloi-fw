019346a6c13cee97f48e1eae46c7a01d7f72ef78
Eloi <eloirobe@gmail.com>
2015-07-14 21:32:31 +0200

    Añadido memcached

--- src/Fw/Components/Cache/MemCache.php
+++ src/Fw/Components/Cache/MemCache.php
@@ -5,20 +5,37 @@ namespace Fw\Components\Cache;
 
 class MemCache implements  Cache{
 
+    private $mem_cache;
+
+    function __construct(\Memcached $mem_cache)
+    {
+        $this->mem_cache=$mem_cache;
+    }
+
     public function set($key, $content, $expiration)
     {
-        // TODO: Implement set() method.
+        $this->mem_cache->set($this->hashing($key),$content,time()+$expiration);
     }
 
     public function get($key)
     {
-        // TODO: Implement get() method.
+        $content=$this->mem_cache->get($this->hashing($key));
+        if (is_null($content)){
+            return false;
+        }else{
+            return $content;
+        }
+
     }
 
     public function delete($key)
     {
-        // TODO: Implement delete() method.
+        $this->mem_cache->delete($this->hashing($key));
     }
 
 
+    private function hashing($key)
+    {
+        return sha1($key);
+    }
 }
